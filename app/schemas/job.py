ae744cc88ee89dabac72125e2decc0289ee72016
abdurwebdev <iabdurrehman2.0@gmail.com>
2026-07-13 19:17:53 +0500

    Fixed Date

--- app/schemas/job.py
+++ app/schemas/job.py
@@ -1,20 +1,23 @@
-from pydantic import BaseModel,ConfigDict
-from typing import Optional,List
+from pydantic import BaseModel, ConfigDict
+from typing import Optional, List
+from datetime import datetime # Import datetime
 
 class JobUIOverviewSchema(BaseModel):
-  model_config = ConfigDict(from_attributes = True)
-  id:int
-  title:str
-  url:str
-  company_name:str
-  company_logo:Optional[str] = None
-  category:str
-  tags:List[str]
-  job_type:str
-  publication_date:str
-  salary:str
-  candidate_required_location:str
-  source:str
-  
+    model_config = ConfigDict(from_attributes=True)
+    
+    id: int
+    title: str
+    url: str
+    company_name: str
+    company_logo: Optional[str] = None
+    category: str
+    tags: List[str]
+    job_type: str
+    # Change this from str to datetime
+    publication_date: Optional[datetime] = None 
+    salary: str
+    candidate_required_location: str
+    source: str
+ 
 class JobDetailOverview(JobUIOverviewSchema):
-  description:Optional[str] = None
+    description: Optional[str] = None
